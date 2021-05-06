6aef1db31087f9794576744f1c46dfe645340310
yibizzi <ibizzi.younes@ine.inpt.ma>
2021-05-06 03:22:23 +0000

    created list of cards component + big card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HeroComponent } from './components/homepage/hero/hero.component';
+import { BigCardWithImageComponent } from './components/homepage/card_image/big-card-with-image/big-card-with-image.component';
+import { ListOfCardsComponent } from './components/homepage/card_image/list-of-cards/list-of-cards.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HeroComponent
+    HeroComponent,
+    ListOfCardsComponent,
+    BigCardWithImageComponent
   ],
   imports: [
     BrowserModule,
