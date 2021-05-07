140964f8334a1831d88a7e3654f36faa80e47fd1
halaouih18 <halaouih18@gmail.com>
2021-05-07 04:15:05 +0000

    Created doctor profile  component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DoctorProfileCardComponent } from './doctor-profile-card/doctor-profile-card.component';
+import { DoctorProfileComponent } from './doctor-profile/doctor-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DoctorProfileCardComponent
+    DoctorProfileCardComponent,
+    DoctorProfileComponent
   ],
   imports: [
     BrowserModule,
