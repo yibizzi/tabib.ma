456bfc761c280f94ce17891a6fd96f979f65e247
halaouih18 <halaouih18@gmail.com>
2021-05-08 00:10:39 +0000

    Created payments-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaymentsListComponent } from './payments-list/payments-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaymentsListComponent
   ],
   imports: [
     BrowserModule,
