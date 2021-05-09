7d80c4816d619b84b14083d129c9acaf29593508
Younes IBIZZI <58573881+yibizzi@users.noreply.github.com>
2021-05-09 02:12:14 +0000

    Create authentication pages (#28)
    * reorganized payment components
    
    * created signup page
    
    * added forget password component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,28 @@
+import { ForgotPasswordPageComponent } from './components/authentication/forgot-password-page/forgot-password-page.component';
 import { LoginPageComponent } from './components/authentication/loginpage/loginpage.component';
 import { HomeBodyComponent } from './components/homepage/home-body/home-body.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignupPageComponent } from './components/authentication/signup-page/signup-page.component';
+import { PaymentFormComponent } from './components/payment/payment-form/payment-form.component';
 
 const routes: Routes = [
   {
     path: '', component: HomeBodyComponent
   },
   {
-    path: "login", component: LoginPageComponent
+    path: "signin", component: LoginPageComponent
   },
-  // {
-  //   path: "**", component: RouteNoteFoundComponent
-  // }
+  {
+    path: "signup", component: SignupPageComponent
+  },
+  {
+    path: "forgotPassword", component: ForgotPasswordPageComponent
+  },
+  {
+    path: "payments", component: PaymentFormComponent
+  },
+  
 ];
 
 @NgModule({
